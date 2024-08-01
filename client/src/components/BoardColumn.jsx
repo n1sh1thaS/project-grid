077a5760b688c7a0eefed539f1ce7bc64ae1f0fd
nselvakumar25 <nselvakumar@ucsd.edu>
2024-07-31 17:00:05 -0700

    implement style changes for drag/drop feature

--- client/src/components/BoardColumn.jsx
+++ client/src/components/BoardColumn.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Typography } from "@mui/material";
 import BoardHeader from "./BoardHeader";
 import TaskCard from "./TaskCard";
@@ -6,6 +6,7 @@ import "../css/board.css";
 
 const BoardColumn = (props) => {
   const { borderColor, taskArr, taskActions, category, boardId } = props;
+  const [dragEnter, setDragEnter] = useState(false);
   let status = "toDo";
   if (category === "In Progress") status = "inProgress";
   if (category === "Done") status = "done";
@@ -13,49 +14,55 @@ const BoardColumn = (props) => {
 
   const handleDrop = (e, newStatus) => {
     e.preventDefault();
+    setDragEnter(false);
     const task = JSON.parse(e.dataTransfer.getData("taskData"));
     taskActions.edit(task.taskId, task.title, task.description, newStatus);
   };
 
   return (
     <Box
+      onDrop={(e) => handleDrop(e, status)}
+      onDragOver={(e) => {
+        e.preventDefault();
+      }}
+      onDragEnter={(e) => setDragEnter(true)}
+      onDragLeave={(e) => setDragEnter(false)}
       className="boardCategory"
       sx={{
-        borderTop: `2px solid ${borderColor}`,
-        borderBottom: `2px solid ${borderColor}`,
+        borderTop: dragEnter
+          ? `4px solid ${borderColor}`
+          : `2px solid ${borderColor}`,
+        borderBottom: dragEnter
+          ? `4px solid ${borderColor}`
+          : `2px solid ${borderColor}`,
       }}
     >
-      <div
-        onDrop={(e) => handleDrop(e, status)}
-        onDragOver={(e) => e.preventDefault()}
+      <Typography
+        variant="h3"
+        align="center"
+        marginTop={2}
+        marginBottom={3}
+        sx={{ fontSize: 25 }}
       >
-        <Typography
-          variant="h3"
-          align="center"
-          marginTop={2}
-          marginBottom={3}
-          sx={{ fontSize: 25 }}
-        >
-          <BoardHeader
-            category={category}
-            boardId={boardId}
-            onAdd={taskActions.add}
+        <BoardHeader
+          category={category}
+          boardId={boardId}
+          onAdd={taskActions.add}
+        />
+        {taskArr.map((task, index) => (
+          <TaskCard
+            key={index}
+            taskId={task.id}
+            taskActions={{
+              delete: taskActions.delete,
+              edit: taskActions.edit,
+            }}
+            title={task.title}
+            description={task.description}
+            status={status}
           />
-          {taskArr.map((task, index) => (
-            <TaskCard
-              key={index}
-              taskId={task.id}
-              taskActions={{
-                delete: taskActions.delete,
-                edit: taskActions.edit,
-              }}
-              title={task.title}
-              description={task.description}
-              status={status}
-            />
-          ))}
-        </Typography>
-      </div>
+        ))}
+      </Typography>
     </Box>
   );
 };
--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -14,45 +14,45 @@ const TaskCard = (props) => {
   const { taskId, title, description, status, taskActions } = props;
 
   return (
-    <Box margin={2} sx={{ width: "97%" }}>
-      <div
-        draggable
-        onDragStart={(e) => {
-          const taskData = {
-            taskId,
-            title,
-            description,
-          };
-          e.dataTransfer.setData("taskData", JSON.stringify(taskData));
-        }}
-      >
-        <Card sx={{ background: "rgb(243 244 246)", width: "97%" }}>
-          <CardContent align="left">
-            <Typography gutterBottom variant="body1" component="div">
-              {title}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {description}
-            </Typography>
-          </CardContent>
-          <CardActions sx={{ justifyContent: "space-between" }}>
-            <EditTaskModal
-              title={title}
-              description={description}
-              status={status}
-              taskId={taskId}
-              onEdit={taskActions.edit}
-            />
-            <IconButton
-              onClick={() => {
-                taskActions.delete(taskId);
-              }}
-            >
-              <DeleteIcon fontSize="small" />
-            </IconButton>
-          </CardActions>
-        </Card>
-      </div>
+    <Box
+      margin={2}
+      sx={{ width: "97%" }}
+      draggable
+      onDragStart={(e) => {
+        const taskData = {
+          taskId,
+          title,
+          description,
+        };
+        e.dataTransfer.setData("taskData", JSON.stringify(taskData));
+      }}
+    >
+      <Card sx={{ background: "rgb(243 244 246)", width: "97%" }}>
+        <CardContent align="left">
+          <Typography gutterBottom variant="body1" component="div">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {description}
+          </Typography>
+        </CardContent>
+        <CardActions sx={{ justifyContent: "space-between" }}>
+          <EditTaskModal
+            title={title}
+            description={description}
+            status={status}
+            taskId={taskId}
+            onEdit={taskActions.edit}
+          />
+          <IconButton
+            onClick={() => {
+              taskActions.delete(taskId);
+            }}
+          >
+            <DeleteIcon fontSize="small" />
+          </IconButton>
+        </CardActions>
+      </Card>
     </Box>
   );
 };
